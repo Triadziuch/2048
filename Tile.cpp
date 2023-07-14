a7eed3e79cbd4e62293cf92003f089e8b59347b5
Triadziuch <kurowski1234567890@gmail.com>
2023-07-14 16:30:10 +0200

    Dodano strukturę Tile

---
+++ Tile.cpp
@@ -0,0 +1,20 @@
+#include "Tile.h"
+
+void Tile::center_origin()
+{
+	this->sprite_tile.setOrigin(this->sprite_tile.getGlobalBounds().left + this->sprite_tile.getGlobalBounds().width / 2.f,
+		this->sprite_tile.getGlobalBounds().top + this->sprite_tile.getGlobalBounds().height / 2.f);
+}
+
+Tile::Tile(int type_, sf::Texture* texture_tile_, float scale_)
+{
+	this->type = type_;
+	this->sprite_tile.setTexture(*texture_tile_);
+	this->center_origin();
+	this->sprite_tile.setScale(scale_, scale_);
+}
+
+void Tile::render(sf::RenderTarget& target)
+{
+	target.draw(this->sprite_tile);
+}
---
+++ Tile.h
@@ -0,0 +1,33 @@
+#include <SFML/Graphics.hpp>
+#include <SFML/Window.hpp>
+#include <SFML/System.hpp>
+#include <vector>
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <Windows.h>
+
+struct Tile {
+private:
+	sf::Sprite		sprite_tile;
+	int				type			= 2;
+
+	void center_origin();
+public:
+	// Constructors / Destructors
+	Tile() {}
+	Tile(int type_, sf::Texture* texture_tile_, float scale_);
+	virtual ~Tile() {};
+
+	// Mutators
+	void setScale(float scale_)				{ this->sprite_tile.setScale(scale_, scale_); }
+	void setTexture(sf::Texture* texture_)  { this->sprite_tile.setTexture(*texture_); this->center_origin(); }
+	void setPosition(sf::Vector2f pos_)		{ this->sprite_tile.setPosition(pos_); }
+	void setType(int type_)					{ this->type = type_; }
+
+	// Accessors
+	const int& getType() const { return this->type; }
+
+	// Rendering tile
+	void render(sf::RenderTarget& target);
+};
