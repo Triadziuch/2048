12866b7de6444cc655b8789168d1d97ecd1013a8
Triadziuch <kurowski1234567890@gmail.com>
2024-10-18 12:53:09 +0200

    Fixed playground

--- src/Playground.cpp
+++ src/Playground.cpp
@@ -2,12 +2,30 @@
 
 Playground::Playground(const sf::Vector2f& windowSize)
 {
-	m_tileMatrix = new TileMatrixModel();
+	// Playground initialization
+	m_texture = &AssetManager::GetTexture("assets/Textures/playground.png");
+	m_sprite.setTexture(*m_texture);
+	m_sprite.setOrigin(m_sprite.getPosition().x + m_sprite.getGlobalBounds().width / 2.f,
+		m_sprite.getPosition().y + m_sprite.getGlobalBounds().height / 2.f);
+	m_sprite.setPosition(windowSize / 2.f);
+	m_scale = (windowSize.y - m_playgroundPadding * 2) / m_sprite.getGlobalBounds().height;
+	m_sprite.setScale(m_scale, m_scale);
+
+	m_innerEdgeWidth *= m_scale;
+	m_outerEdgeWidth *= m_scale;
+	m_tileWidth *= m_scale;
+
+	sf::Vector2f playground_pos = m_sprite.getPosition();
+	playground_pos.x -= m_sprite.getLocalBounds().width / 4.f;
+	playground_pos.y -= m_sprite.getLocalBounds().height / 4.f;
+	m_tileMatrix = new TileMatrix(&m_scale, &m_outerEdgeWidth, &m_innerEdgeWidth, &m_tileWidth, playground_pos);
 	m_tileMatrix->spawn(2);
+	m_gui = new GUI(windowSize, m_sprite.getGlobalBounds());
 }
 
 Playground::~Playground()
 {
+	delete m_gui;
 	delete m_tileMatrix;
 }
 
@@ -19,7 +37,11 @@ void Playground::update(float dt)
 	if (m_tileMatrix->getIsGameOver()) {
 		if (!m_isGameOver) {
 			m_isGameOver = true;
+			m_gui->startGameOver();
+			m_gui->saveBestScore();
 		}
+
+		m_gui->updateGameOver(dt);
 	}
 }
 
@@ -28,6 +50,7 @@ void Playground::updateScore()
 	const int added_m_score = m_tileMatrix->getAddedScore();
 	m_tileMatrix->setAddedScore(0);
 	m_score += added_m_score;
+	m_gui->addScore(added_m_score);
 }
 
 void Playground::move(const sf::Keyboard::Key key)
@@ -36,28 +59,39 @@ void Playground::move(const sf::Keyboard::Key key)
 
 	switch (key)
 	{
-		case sf::Keyboard::Key::Up:
-		case sf::Keyboard::Key::W:
-			m_tileMatrix->moveUp();
-			break;
-		case sf::Keyboard::Key::Down:
-		case sf::Keyboard::Key::S:
-			m_tileMatrix->moveDown();
-			break;
-		case sf::Keyboard::Key::Left:
-		case sf::Keyboard::Key::A:
-			m_tileMatrix->moveLeft();
-			break;
-		case sf::Keyboard::Key::Right:
-		case sf::Keyboard::Key::D:
-			m_tileMatrix->moveRight();
-			break;
+	case sf::Keyboard::Key::Up:
+	case sf::Keyboard::Key::W:
+		m_tileMatrix->moveUp();
+		break;
+	case sf::Keyboard::Key::Down:
+	case sf::Keyboard::Key::S:
+		m_tileMatrix->moveDown();
+		break;
+	case sf::Keyboard::Key::Left:
+	case sf::Keyboard::Key::A:
+		m_tileMatrix->moveLeft();
+		break;
+	case sf::Keyboard::Key::Right:
+	case sf::Keyboard::Key::D:
+		m_tileMatrix->moveRight();
+		break;
 	}
 }
 
 void Playground::clearBoard()
 {
 	m_isGameOver = false;
+	m_gui->stopGameOver();
+	m_gui->saveBestScore();
+	m_gui->setScore(0);
 	m_tileMatrix->clearBoard();
 	m_tileMatrix->spawn(2);
+}
+
+void Playground::render(sf::RenderTarget& target)
+{
+	target.clear(m_backgroundColor);
+	target.draw(m_sprite);
+	m_tileMatrix->render(target);
+	m_gui->render(target);
 }
